0e5c5b751cf5d73691d18d8dcc2d9a69c8da9931
lorelairaras <lorelairaras@gmail.com>
2025-08-28 23:47:46 +0800

    made better changes to the design

--- docs/src/main.js
+++ docs/src/main.js
@@ -1,24 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const menuBtn = document.getElementById("menu-btn");
-  const mobileMenu = document.getElementById("mobile-menu");
+const menuBtn = document.getElementById("menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+const closeBtn = document.getElementById("close-menu");
 
-  if (menuBtn && mobileMenu) {
-    menuBtn.addEventListener("click", () => {
-      const isOpen = mobileMenu.classList.contains("hidden");
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.remove("hidden");
+});
 
-      if (isOpen) {
-        // Open
-        mobileMenu.classList.remove("hidden");
-        document.body.classList.add("overflow-hidden");
-        menuBtn.classList.add("menu-open");
-        menuBtn.setAttribute("aria-expanded", "true");
-      } else {
-        // Close
-        mobileMenu.classList.add("hidden");
-        document.body.classList.remove("overflow-hidden");
-        menuBtn.classList.remove("menu-open");
-        menuBtn.setAttribute("aria-expanded", "false");
-      }
-    });
-  }
+closeBtn.addEventListener("click", () => {
+  mobileMenu.classList.add("hidden");
 });
